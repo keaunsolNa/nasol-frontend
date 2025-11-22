28c6bc9fee8e58bab50fa2bbdef0d2ddfe1c7830
[gyparkdev] <[gyparkdev@gmail.com]>
2025-11-22 15:49:27 +0900

    feat: [나큰솔] 상단 Navbar를 통해 소셜 로그인을 할 수 있다 [NSF-4]

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 // app/layout.tsx
 import './globals.css';
+import { AuthProvider } from '@/contexts/AuthContext';
+import Navbar from '../components/Navbar';
 
 export const metadata = {
     title: 'MyApp',
@@ -9,6 +11,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <body>
+                <AuthProvider>
+                    <Navbar />
+                    {children}
+                </AuthProvider>
             </body>
         </html>
     );
